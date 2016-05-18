96063552c886d4c2df75bd82920f37a283601946
Christopher Motes <motescj@aim.com>
2016-05-18 03:37:42 +0000

    fixed as external fact

--- aws_tags.rb
+++ aws_tags.rb
@@ -3,5 +3,8 @@ require 'open3'
 RESOURCEID = `curl  -s http://169.254.169.254/latest/meta-data/instance-id`
 cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
 Open3.popen3(cmd) do |stdin, stdout, stderr, status, thread|
-	puts 'data' + stdout.read
+	while line = stdout.gets
+		split = line.split
+		puts 'aws_' + split[3] + ' => ' + split[4]
+	end
 end
