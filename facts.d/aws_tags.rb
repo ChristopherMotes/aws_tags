5e5f92e77662feac3511d1d2edda9928ba50db25
Christopher Motes <motescj@aim.com>
2016-10-18 07:52:03 -0500

    fixed ruby for puppet 4

--- facts.d/aws_tags.rb
+++ facts.d/aws_tags.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby 
+#!/opt/puppetlabs/bin/ruby 
 require 'open3'
 RESOURCEID = `curl  -s http://169.254.169.254/latest/meta-data/instance-id`
 cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
