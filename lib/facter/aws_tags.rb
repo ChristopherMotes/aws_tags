2b1d640adfa098073c550242bdcbadda39dde9fe
EC2 Default User <ec2-user@ip-192-168-0-52.ec2.internal>
2016-10-27 15:37:07 +0000

    fixed plugin naming structure

--- lib/facter/aws_tags.rb
+++ lib/facter/aws_tags.rb
@@ -5,11 +5,12 @@ Facter.add(:aws_tags) do
 	setcode do
 		tags_hash =  {}
 		RESOURCEID = `curl  -s http://169.254.169.254/latest/meta-data/instance-id`
-		cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
+#		cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
+		cmd = "/usr/local/bin/aws ec2 describe-tags  --region us-east-1 --output text --filter \"Name=resource-id,Values=#{RESOURCEID}\" "
 		Open3.popen3(cmd) do |stdin, stdout, stderr, status, thread|
 			while line = stdout.gets
 				split = line.split
-				tags_hash[split[3]] = split[4]
+				tags_hash[split[1]] = split[4]
 			end
 		end
 		tags_hash
