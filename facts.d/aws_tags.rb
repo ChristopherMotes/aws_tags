efa42a711775fe5374f1c35f5014f579db8f4d8a
EC2 Default User <ec2-user@ip-192-168-0-52.ec2.internal>
2016-10-27 15:18:34 +0000

    moved to aws cli

--- facts.d/aws_tags.rb
+++ facts.d/aws_tags.rb
@@ -1,10 +1,12 @@
 #!/opt/puppetlabs/puppet/bin/ruby 
 require 'open3'
 RESOURCEID = `curl  -s http://169.254.169.254/latest/meta-data/instance-id`
-cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
+#cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
+cmd = "/usr/local/bin/aws ec2 describe-tags  --region us-east-1 --output text --filter \"Name=resource-id,Values=#{RESOURCEID}\" "
 Open3.popen3(cmd) do |stdin, stdout, stderr, status, thread|
+		print stdout.gets
 	while line = stdout.gets
 		split = line.split
-		print 'aws_' + split[3] + '=' + split[4] + "\n"
+		print 'aws_' + split[1] + '=' + split[4] + "\n"
 	end
 end
