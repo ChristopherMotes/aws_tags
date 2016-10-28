b58642851eaea754e27742a92a7d89e405e218c8
Christopher Motes <ec2-user@ip-10-0-0-30.ec2.internal>
2016-10-28 13:36:41 +0000

    again, path statement

--- lib/facter/aws_tags.rb
+++ lib/facter/aws_tags.rb
@@ -1,12 +1,13 @@
 # Your ec2 instance's AIM role must have at least AmazonEC2ReadOnlyAccess. 
 # DO NOT use keys. Eventually, I'll error trap for this.
 require 'open3'
+ENV['PATH'] = '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/aws/bin:/opt/puppetlabs/bin'
 Facter.add(:aws_tags) do
 	setcode do
 		tags_hash =  {}
 		RESOURCEID = `curl  -s http://169.254.169.254/latest/meta-data/instance-id`
 #		cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
-		cmd = "/usr/local/bin/aws ec2 describe-tags  --region us-east-1 --output text --filter \"Name=resource-id,Values=#{RESOURCEID}\" "
+		cmd = "aws ec2 describe-tags  --region us-east-1 --output text --filter \"Name=resource-id,Values=#{RESOURCEID}\" "
 		Open3.popen3(cmd) do |stdin, stdout, stderr, status, thread|
 			while line = stdout.gets
 				split = line.split
