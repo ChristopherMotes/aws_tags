c936ffe8b3d0b8c5fca5be7a047e692a156e28f8
Christopher Motes <ec2-user@ip-10-0-0-30.ec2.internal>
2016-10-28 13:33:59 +0000

    removed bad line; change to path variable

--- facts.d/aws_tags.rb
+++ facts.d/aws_tags.rb
@@ -1,10 +1,10 @@
 #!/opt/puppetlabs/puppet/bin/ruby 
 require 'open3'
+ENV['PATH'] = '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/aws/bin:/opt/puppetlabs/bin'
 RESOURCEID = `curl  -s http://169.254.169.254/latest/meta-data/instance-id`
 #cmd = "ec2-describe-tags --filter \"resource-id=#{RESOURCEID}\""
-cmd = "/usr/local/bin/aws ec2 describe-tags  --region us-east-1 --output text --filter \"Name=resource-id,Values=#{RESOURCEID}\" "
+cmd = "aws ec2 describe-tags  --region us-east-1 --output text --filter \"Name=resource-id,Values=#{RESOURCEID}\" "
 Open3.popen3(cmd) do |stdin, stdout, stderr, status, thread|
-		print stdout.gets
 	while line = stdout.gets
 		split = line.split
 		print 'aws_' + split[1] + '=' + split[4] + "\n"
